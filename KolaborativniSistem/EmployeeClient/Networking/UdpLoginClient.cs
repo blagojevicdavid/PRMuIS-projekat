e426deab29edffeb00ead00c1d131c878c69fe6c
Marija <marija6804@gmail.com>
2026-01-25 11:47:36 +0100

    logovanje zaposlenog

---
+++ KolaborativniSistem/EmployeeClient/Networking/UdpLoginClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeClient.Networking
+{
+    public class UdpLoginClient
+    {
+        private readonly string _serverIp = "127.0.0.1";
+        private readonly int _serverPort = 5000;
+
+        public bool Login(string username, string password)
+        {
+            using (UdpClient client = new UdpClient())
+            {
+                string message = $"ZAPOSLENI|{username}|{password}";   //treba li uopste password?
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                client.Send(data, data.Length, _serverIp, _serverPort);
+
+                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] responseData = client.Receive(ref remoteEndPoint);
+                string response = Encoding.UTF8.GetString(responseData);
+
+                return response == "OK";
+            }
+        }
+    }
+}
---
+++ KolaborativniSistem/EmployeeClient/ViewModels/LoginViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeClient.ViewModels;
+
+ public sealed class LoginViewModel : INotifyPropertyChanged
+{
+    private string _serverIp = "127.0.0.1";
+    private int _udpPort = 50032;
+    private string _username = "";   //jel ovo ok???
+
+    public string ServerIp
+    {
+        get => _serverIp;
+        set
+        {
+            _serverIp = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int UdpPort
+    {
+        get => _udpPort;
+        set
+        {
+            _udpPort = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Username
+    {
+        get => _username;
+        set
+        {
+            _username = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+
