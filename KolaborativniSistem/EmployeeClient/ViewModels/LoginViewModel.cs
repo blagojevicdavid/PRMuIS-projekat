c21dc5bb782cdb288899a7cf473330db3cf9d4b6
Marija <marija6804@gmail.com>
2026-01-26 16:12:17 +0100

    Izmjene u LoginViewModel

--- KolaborativniSistem/EmployeeClient/ViewModels/LoginViewModel.cs
+++ KolaborativniSistem/EmployeeClient/ViewModels/LoginViewModel.cs
@@ -1,56 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EmployeeClient.ViewModels;
 
- public sealed class LoginViewModel : INotifyPropertyChanged
+public sealed class LoginViewModel : INotifyPropertyChanged
 {
     private string _serverIp = "127.0.0.1";
     private int _udpPort = 50032;
-    private string _username =
-    EmployeeClient.Properties.Settings.Default.LastEmployeeUsername ?? "";
+    private string _username = EmployeeClient.Properties.Settings.Default.LastEmployeeUsername ?? "";
+    private string _loginHint = "";
 
     public string ServerIp
     {
         get => _serverIp;
-        set
-        {
-            _serverIp = value;
-            OnPropertyChanged();
-        }
+        set { _serverIp = value; OnPropertyChanged(); }
     }
 
     public int UdpPort
     {
         get => _udpPort;
-        set
-        {
-            _udpPort = value;
-            OnPropertyChanged();
-        }
+        set { _udpPort = value; OnPropertyChanged(); }
     }
 
     public string Username
     {
         get => _username;
-        set
-        {
-            _username = value;
-            OnPropertyChanged();
-        }
+        set { _username = value; OnPropertyChanged(); }
+    }
+
+    public string LoginHint
+    {
+        get => _loginHint;
+        set { _loginHint = value; OnPropertyChanged(); }
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
-
     private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
-
-
