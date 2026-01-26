6bcf9023a87f2db88fb4a91eaaea8fad57e8dbe9
Marija <marija6804@gmail.com>
2026-01-26 16:09:13 +0100

    Dodala EmployeeDashboardViewModel.cs

---
+++ KolaborativniSistem/EmployeeClient/ViewModels/EmployeeDashboardViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EmployeeClient.ViewModels;
+
+public sealed class EmployeeDashboardViewModel : INotifyPropertyChanged
+{
+    private string _username = "";
+    private string _connectionStatus = "";
+    private TaskRow? _selectedTask;
+    private string _formHint = "";
+
+    public string Username
+    {
+        get => _username;
+        set { _username = value; OnPropertyChanged(); }
+    }
+
+    public string ConnectionStatus
+    {
+        get => _connectionStatus;
+        set { _connectionStatus = value; OnPropertyChanged(); }
+    }
+
+    public ObservableCollection<TaskRow> AssignedTasks { get; } = new();
+
+    public TaskRow? SelectedTask
+    {
+        get => _selectedTask;
+        set { _selectedTask = value; OnPropertyChanged(); }
+    }
+
+    public string FormHint
+    {
+        get => _formHint;
+        set { _formHint = value; OnPropertyChanged(); }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged([CallerMemberName] string? name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
+
+public sealed class TaskRow
+{
+    public string Naziv { get; set; } = "";
+    public string Menadzer { get; set; } = "";
+    public string Rok { get; set; } = "";
+    public string Prioritet { get; set; } = "";
+    public string Status { get; set; } = "";
+}
